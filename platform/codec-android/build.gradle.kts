4f3a160fe4dd4b8a05451b493887fbd0003bae99
Antonio Falcirolli <antonio.falcirolli@gmail.com>
2026-09-08 16:49:31 -0300

    feat(codec-android): configure Android codec bridge module

---
+++ platform/codec-android/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "studio.guitarlab.platform.codec.android"
+    compileSdk = 36
+
+    defaultConfig {
+        minSdk = 26
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+dependencies {
+    implementation(project(":core:codec"))
+}
