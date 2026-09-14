bcbebbcfd0a02264bf46a29eceacee7015c6c3a9
Antonio Falcirolli <antonio.falcirolli@gmail.com>
2026-09-14 11:37:56 -0300

    chore: promote practice workflow refinement to 0.5.0-rc3 [skip ci]

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "studio.guitarlab.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 22
-        versionName = "0.5.0-rc2"
+        versionCode = 23
+        versionName = "0.5.0-rc3"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
